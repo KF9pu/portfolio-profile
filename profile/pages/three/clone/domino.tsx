d495caf1c9c3be9c3b2818851d0461fab65a786f
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2023-07-31 14:45:06 +0900

    chore : clean up code
    - Delete unused code
    - Move code to matching folder

--- profile/pages/three/clone/domino.tsx
+++
@@ -1,7 +0,0 @@
-import DominoCanvas from "@/groups/domino/components/DominoCanvas";
-import type { NextPage } from "next";
-const Domino: NextPage = () => {
-  return <DominoCanvas>ss</DominoCanvas>;
-};
-
-export default Domino;
