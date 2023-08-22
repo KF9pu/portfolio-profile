a914a395174e759f7d7d09964f37b1ad477b7b42
hong seong hwa <pushan45hsh@gmail.com>
2023-08-23 06:28:40 +0900

    feat : Mepage 신상정보 완료

--- profile/src/groups/threeHome/components/MePage.tsx
+++ profile/src/groups/threeHome/components/MePage.tsx
@@ -1,20 +1,76 @@
 import type { FC } from "react";
+import React from "react";
+import Title from "./Title";
+import useMyinfo from "@/common/hooks/useMyinfo";
+import { cls } from "@/common/libs";
+import { NoneProps, OnlyChildrenProps } from "@/common/interface";
+import { PresonalInfoProps, SectionProps } from "../interface/MePage";
 
 interface MePageProps {}
 
 const MePage: FC<MePageProps> = ({}) => {
+  const { yearsOfService, hobys, name, bloodType, residence, mbti, gender, color, age, born } = useMyinfo();
   return (
-    <div>
-      <Section>1</Section>
-      <Section>2</Section>
-      <Section>3</Section>
-      <Section>4</Section>
-    </div>
+    <Main>
+      <Section>
+        <Title big>자기소개</Title>
+        <Underline />
+      </Section>
+
+      <Section tab>
+        <Title normal>✔ 신상정보</Title>
+        <Contents>
+          <PresonalInfo title="이름 : " data={name} />
+          <PresonalInfo title="생년월일 : " data={born + ` (${age})`} smallData />
+          <PresonalInfo title="성별 : " data={gender} />
+          <PresonalInfo title="혈액형 : " data={bloodType} />
+          <PresonalInfo title="MBTI : " data={mbti} small smallData />
+          <PresonalInfo title="Color : " data={color} small smallData />
+          <PresonalInfo title="거주지 : " data={residence} />
+          <PresonalInfo title="취미 : " data={hobys} />
+        </Contents>
+      </Section>
+
+      <Section tab>
+        <Title normal>✔ 연혁</Title>
+        <Contents>
+          <button className="text-[14px]">[ 보기 ]</button>
+        </Contents>
+      </Section>
+
+      <Section tab>
+        <Title normal>✔ 자기소개서</Title>
+        <Contents>
+          <button className="text-[14px]">[ 보기 ]</button>
+        </Contents>
+      </Section>
+    </Main>
   );
 };
 export default MePage;
 
-interface SectionProps {}
-const Section: FC<SectionProps> = ({}) => {
-  return <section></section>;
+const Main: FC<OnlyChildrenProps> = ({ children }) => {
+  return <div className={cls("flex flex-col gap-[8px] md:gap-[12px]", "text-secondary")}>{children}</div>;
+};
+
+const Section: FC<SectionProps> = ({ children, tab }) => {
+  return <section className={cls("flex flex-col md:gap-[4px]", tab ? "pl-[4px]" : "")}>{children}</section>;
+};
+
+const Underline: FC<NoneProps> = () => {
+  return <hr className={cls("w-[92%] h-px", "bg-primary", "border-0", "mb-[4px]")} />;
+};
+
+const Contents: FC<OnlyChildrenProps> = ({ children }) => {
+  return <div className="pl-[8px] md:pl-[12px]">{children}</div>;
+};
+
+const PresonalInfo: FC<PresonalInfoProps> = ({ title, data, small, smallData }) => {
+  return (
+    <div className={cls("text-primary", small ? "text-[14px]" : "")}>
+      <span>{title}</span>
+
+      <span className={cls("text-secondary", smallData ? "text-[12px]" : "text-[14px]")}>{data}</span>
+    </div>
+  );
 };
