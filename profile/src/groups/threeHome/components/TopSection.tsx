da09f8a16db626c6dc2ed40c4d7fbd0881e47db0
hong seong hwa <pushan45hsh@gmail.com>
2023-10-06 07:58:42 +0900

    feat : modal 중첩 되는 UX 수정

--- profile/src/groups/threeHome/components/TopSection.tsx
+++ profile/src/groups/threeHome/components/TopSection.tsx
@@ -1,6 +1,13 @@
 import { cls } from "@/common/libs";
 import useTheme from "@/groups/theme/useTheme";
-import { _careerIsOpen, _historyIsOpen, _sectionIsOpen, _sectionNum, _selfIntroductionIsOpen } from "@/store/default";
+import {
+  _careerIsOpen,
+  _historyIsOpen,
+  _isDropDown,
+  _sectionIsOpen,
+  _sectionNum,
+  _selfIntroductionIsOpen,
+} from "@/store/default";
 import { useEffect, type FC } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import PageByNumber from "./PageByNumber";
@@ -17,6 +24,7 @@ const TopSection: FC<TopSectionProps> = ({}) => {
   const historyIsOpen = useRecoilValue(_historyIsOpen);
   const selfIntroductionIsOpen = useRecoilValue(_selfIntroductionIsOpen);
   const careerIsOpen = useRecoilValue(_careerIsOpen);
+  const [IsDropDown, setIsDropDown] = useRecoilState(_isDropDown);
 
   useEffect(() => {
     console.log("historyIsOpen : ", historyIsOpen);
@@ -28,25 +36,31 @@ const TopSection: FC<TopSectionProps> = ({}) => {
 
   return (
     <div className={cls("relative", "font-DNF", "select-none", `theme-${ThemeCode}`)}>
-      <button
-        className={cls(
-          pageNum === 0 ? "translate-x-[calc(100%+20px)]" : "translate-x-[-5vw] md:translate-x-0",
-          "fixed right-0 top-[65px] md:right-[20px] z-[100]",
-          "w-[90vw] md:w-[30vw] md:min-w-[320px] md:max-w-[420px]",
-          "border border-primary rounded-xl",
-          "bg-secondary hover:bg-tertiary",
-          "text-quaternary hover:text-primary",
-          "py-[10px]",
-          "select-none",
-          "transition-all duration-500"
-        )}
-        onClick={() => {
-          SetIsOpen(prev => !prev);
-        }}
-      >
-        {isOpen ? "닫기" : "열기"}
-      </button>
-      <PageByNumber />
+      {
+        <button
+          className={cls(
+            pageNum === 0
+              ? "translate-x-[calc(100%+20px)]"
+              : !IsDropDown
+              ? "translate-x-[-5vw] md:translate-x-0"
+              : "translate-x-[calc(100%+20px)]",
+            "fixed right-0 top-[65px] md:right-[20px] z-[100]",
+            "w-[90vw] md:w-[30vw] md:min-w-[320px] md:max-w-[420px]",
+            "border border-primary rounded-xl",
+            "bg-secondary hover:bg-tertiary",
+            "text-quaternary hover:text-primary",
+            "py-[10px]",
+            "select-none",
+            "transition-all duration-500"
+          )}
+          onClick={() => {
+            SetIsOpen(prev => !prev);
+          }}
+        >
+          {isOpen ? "닫기" : "열기"}
+        </button>
+      }
+      {!IsDropDown ? <PageByNumber /> : null}
       {historyIsOpen ? <MyPageHistory /> : null}
       {selfIntroductionIsOpen ? <MyPageIntroduction /> : null}
       {careerIsOpen ? <MyPageCarrer /> : null}
