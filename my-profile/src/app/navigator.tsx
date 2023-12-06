9547f4f181c9d2bf2893f036f6a37f0d41479bb0
hong seong hwa <pushan45hsh@gmail.com>
2023-12-06 17:41:55 +0900

    feat : navigator 완ㄹ쇼

--- my-profile/src/app/navigator.tsx
+++ my-profile/src/app/navigator.tsx
@@ -2,11 +2,12 @@
 
 import { cls } from "hsh-utils-string";
 import type { FC } from "react";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import { _isDropDown } from "./recoilContextProvider";
 import { useGesture } from "@use-gesture/react";
 import { animated, useSpring, useSprings, a } from "@react-spring/web";
-import floatingButtonColors from "@/constants/floatingButtonColors";
+import floatingButtonCostans from "@/constants/floatingButtonCostans";
+import { useRouter } from "next/navigation";
 
 interface navigatorProps {}
 
@@ -15,8 +16,9 @@ const Navigator: FC<navigatorProps> = ({}) => {
   const avatarRefs = useRef<HTMLDivElement[]>([]);
   const avatarRefInitialPositions = useRef<number[]>([]);
   const containerRef = useRef<HTMLDivElement>(null!);
-
   const isVisible = useRef(false);
+  const [floatingFlipped, setFloatingFlipped] = useState(false);
+  const [menuFlipped, setMenuFlipped] = useState(false);
 
   const [{ x, y, opacity }, api] = useSpring(
     () => ({
@@ -28,7 +30,7 @@ const Navigator: FC<navigatorProps> = ({}) => {
   );
 
   const [avatarSprings, avatarApi] = useSprings(
-    floatingButtonColors.length,
+    floatingButtonCostans.length,
     i => ({
       y: 0,
       opacity: 0,
@@ -56,7 +58,7 @@ const Navigator: FC<navigatorProps> = ({}) => {
   const avatarTimeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
   const bindGestures = useGesture({
-    onClick: ({ event }) => {
+    onClick: () => {
       if (!isVisible.current) {
         if (backgroundTimeoutRef.current) {
           clearTimeout(backgroundTimeoutRef.current);
@@ -74,6 +76,7 @@ const Navigator: FC<navigatorProps> = ({}) => {
         avatarApi.start({
           y: 0,
         });
+        setFloatingFlipped(true);
       } else {
         api.start({
           opacity: 0,
@@ -85,6 +88,7 @@ const Navigator: FC<navigatorProps> = ({}) => {
             isVisible.current = false;
           },
         }));
+        setFloatingFlipped(false);
       }
     },
     onHover: ({ hovering }) => {
@@ -105,6 +109,8 @@ const Navigator: FC<navigatorProps> = ({}) => {
         avatarApi.start({
           y: 0,
         });
+
+        setFloatingFlipped(true);
       } else {
         backgroundTimeoutRef.current = setTimeout(() => {
           api.start({
@@ -119,6 +125,7 @@ const Navigator: FC<navigatorProps> = ({}) => {
               isVisible.current = false;
             },
           }));
+          setFloatingFlipped(false);
         }, 650);
       }
     },
@@ -165,41 +172,40 @@ const Navigator: FC<navigatorProps> = ({}) => {
       >
         <animated.button
           className={cls(
-            "w-[calc(100% - 16px)] h-[17px]",
-            "rounded-[8px]",
-            "bg-[#cccccc33]",
-            "mx-[8px] mb-[4px]",
-            "rotate-90",
-            "z-[100]"
+            "w-full h-[56px]",
+            "p-[12px]",
+            "bg-white",
+            "rounded-full"
           )}
+          onClick={() => setMenuFlipped(prev => !prev)}
           style={{ opacity }}
         >
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5.5 4.625C6.12132 4.625 6.625 4.12132 6.625 3.5C6.625 2.87868 6.12132 2.375 5.5 2.375C4.87868 2.375 4.375 2.87868 4.375 3.5C4.375 4.12132 4.87868 4.625 5.5 4.625ZM9.5 4.625C10.1213 4.625 10.625 4.12132 10.625 3.5C10.625 2.87868 10.1213 2.375 9.5 2.375C8.87868 2.375 8.375 2.87868 8.375 3.5C8.375 4.12132 8.87868 4.625 9.5 4.625ZM10.625 7.5C10.625 8.12132 10.1213 8.625 9.5 8.625C8.87868 8.625 8.375 8.12132 8.375 7.5C8.375 6.87868 8.87868 6.375 9.5 6.375C10.1213 6.375 10.625 6.87868 10.625 7.5ZM5.5 8.625C6.12132 8.625 6.625 8.12132 6.625 7.5C6.625 6.87868 6.12132 6.375 5.5 6.375C4.87868 6.375 4.375 6.87868 4.375 7.5C4.375 8.12132 4.87868 8.625 5.5 8.625ZM10.625 11.5C10.625 12.1213 10.1213 12.625 9.5 12.625C8.87868 12.625 8.375 12.1213 8.375 11.5C8.375 10.8787 8.87868 10.375 9.5 10.375C10.1213 10.375 10.625 10.8787 10.625 11.5ZM5.5 12.625C6.12132 12.625 6.625 12.1213 6.625 11.5C6.625 10.8787 6.12132 10.375 5.5 10.375C4.87868 10.375 4.375 10.8787 4.375 11.5C4.375 12.1213 4.87868 12.625 5.5 12.625Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            />
-          </svg>
+          <div
+            className={cls(
+              "w-full h-full",
+              "bg-[url('/images/exchange.png')] bg-contain bg-center bg-no-repeat"
+            )}
+          />
         </animated.button>
         {avatarSprings.map((springs, index) => {
+          const { front: frontConstant, back: backConstant } =
+            floatingButtonCostans[index];
           return (
             <animated.div
-              key={`color${floatingButtonColors[index]}${index}`}
+              key={`avatarSprings${index}`}
               ref={ref => (avatarRefs.current[index] = ref!)}
-              className={cls("w-[56px] h-[56px]", "rounded-full", "")}
+              className={cls("w-[56px] h-[56px]", "rounded-full")}
               style={{
                 ...springs,
-                backgroundColor: floatingButtonColors[index],
               }}
-            ></animated.div>
+            >
+              <MenuReverseDiv flipped={menuFlipped} index={index}>
+                {backConstant.title}
+              </MenuReverseDiv>
+              <MenuReverseDiv front flipped={menuFlipped} index={index}>
+                {frontConstant.title}
+              </MenuReverseDiv>
+            </animated.div>
           );
         })}
 
@@ -222,19 +228,92 @@ const Navigator: FC<navigatorProps> = ({}) => {
             ),
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="#1a1a1a"
-            viewBox="0 0 256 256"
-          >
-            <rect width="256" height="256" fill="none"></rect>
-            <path d="M128,24A104,104,0,0,0,36.8,178l-8.5,29.9a16.1,16.1,0,0,0,4,15.8,15.8,15.8,0,0,0,15.7,4l30-8.5A104,104,0,1,0,128,24Zm32,128H96a8,8,0,0,1,0-16h64a8,8,0,0,1,0,16Zm0-32H96a8,8,0,0,1,0-16h64a8,8,0,0,1,0,16Z"></path>
-          </svg>
+          <FlootingButtonReverseDiv flipped={floatingFlipped} front />
+          <FlootingButtonReverseDiv flipped={floatingFlipped} />
         </animated.div>
       </animated.div>
     </>
   );
 };
 export default Navigator;
+
+interface FlootingButtonReverseDivProps {
+  flipped: boolean;
+  front?: true;
+}
+
+const FlootingButtonReverseDiv: FC<FlootingButtonReverseDivProps> = ({
+  flipped,
+  front,
+}) => {
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  return (
+    <a.div
+      className={cls(
+        "absolute",
+        "w-[40px] h-[40px]",
+        "bg-cover bg-no-repeat bg-center",
+        "will-change-transform",
+        "cursor-pointer",
+        !front
+          ? "rotate-180 bg-[url('/images/menu.png')]"
+          : "bg-[url('/images/close.png')]"
+      )}
+      style={{ opacity: front ? opacity : opacity.to(o => 1 - o), transform }}
+    />
+  );
+};
+
+interface MenuReverseDivProps {
+  flipped: boolean;
+  front?: true;
+  index: number;
+  children: React.ReactNode;
+}
+
+const MenuReverseDiv: FC<MenuReverseDivProps> = ({
+  flipped,
+  front,
+  index,
+  children,
+}) => {
+  const router = useRouter();
+
+  const { color, routePath } = front
+    ? floatingButtonCostans[index].front
+    : floatingButtonCostans[index].back;
+
+  const { transform, opacity, backgroundColor } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    backgroundColor: color,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  return (
+    <a.div
+      className={cls(
+        "absolute",
+        "flex justify-center items-center",
+        "w-[56px] h-[56px]",
+        "rounded-full",
+        "will-change-transform",
+        "cursor-pointer"
+      )}
+      style={{
+        opacity: front ? opacity : opacity.to(o => 1 - o),
+        transform,
+        backgroundColor,
+        rotateX: front ? "180deg" : "0deg",
+      }}
+      onClick={() => router.push(routePath)}
+    >
+      {children}
+    </a.div>
+  );
+};
---
+++ my-profile/src/constants/floatingButtonCostans.ts
@@ -0,0 +1,20 @@
+const floatingButtonCostans = [
+  {
+    front: { title: "Home", color: "#669EF2", routePath: "/" },
+    back: { title: "준비중", color: "#000000", routePath: "/" },
+  },
+  {
+    front: { title: "Gallery", color: "#F9DB6D", routePath: "/gallery" },
+    back: { title: "준비중", color: "#000000", routePath: "/" },
+  },
+  {
+    front: { title: "MBTI", color: "#DC602E", routePath: "/mbti" },
+    back: { title: "준비중", color: "#000000", routePath: "/" },
+  },
+  {
+    front: { title: "LOL", color: "#83BB70", routePath: "/lol" },
+    back: { title: "준비중", color: "#000000", routePath: "/" },
+  },
+];
+
+export default floatingButtonCostans;
